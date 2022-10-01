f90143cdafd662101260647e8d981cccd8ef4e7a
jungshiwoo1011 <codplay.shiwoo@gmail.com>
2022-10-01 15:55:58 +0900

    oijoihoj

--- python_basics/06_lotto.py
+++ python_basics/06_lotto.py
@@ -11,4 +11,5 @@ for j in range(7):
     pick = lotto_numbers.pop (random.randint(0, len(lotto_numbers) - 1))
     lotto_picks.append(pick)
 
+
 print(f"이번 주 1등 당첨 예상번호는 {lotto_picks} 입니다. 행운을 빕니다.")
