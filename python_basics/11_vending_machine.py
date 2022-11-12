674a7ffb975c147475c8fd9823f086ed63966b39
jungshiwoo1011 <codeplay.shiwoo@gmail.com>
2022-11-12 16:21:23 +0900

    1112 ㄱ미선욱 시우 김시우 결섯

--- python_basics/11_vending_machine.py
+++ python_basics/11_vending_machine.py
@@ -1,14 +1,26 @@
-drinks = {"콜라" : 2000, "환타" : 3000, "박카스" : 1500, "레드볼" : 4000, "칵테일" : 30000}
+drinks = {"갈아만든 사과" : [4500, 10], "콜라" : [2000, 10],  "환타" : [3000, 10], "박카스" : [1500, 10], "레드볼" : [4000, 10], "칵테일" : [30000, 10]}
 money = 0
 customer = True
 select = 0
 choice = 0
 income = 0
+name = []
+price = []
+price_stock = []
+stock = []
+name = list(drinks.keys()) 
+price_stock = list(drinks.values())
+for i in price_stock:
+    price.append(i[0])
+    price.append(i[1]) 
 
 def display():
     print("*" * 22)
-    print("*" * 5, "독약자판기", "*" * 5)
-    print(drinks)
+    print("*" * 5, "독약자판기", "*" * 5) 
+    for j in range(len(name)):
+        print(f"{name[0]} - {price[0]}원")
+    print(name)
+    print(price)
     print("*" * 22)
     print(f"잔액 : {money}")
     print("*" * 22)
@@ -24,11 +36,16 @@ while True:
             continue
         elif select == 2:
             display()
-            choice = input("음료선택 : ")
-            if money >= drinks[choice]:
-                print(f",{choice} 음료구매 완료 ")
-                income += drinks[choice]
-                money = money - drinks[choice]
+            choice = name.index(input("음료선택 : "))
+            if money >= price[choice]:
+                if stock[choice] > 0:
+                    print(f"<{name[choice]}> 음료구매 완료 ")
+                    income += price[choice]
+                    money = money - price[choice]
+                    stock[choice] -= 1
+                else:
+                    print(f"{name[choice]} 음료는 품절입니다")
+                    break
             else:
                 print("잔액부족")
                 break
@@ -39,7 +56,10 @@ while True:
                 money = 0
                 customer = False
         elif select == 1470:
-            print(f"오늘의 수입 : {income}")
+            print(f"오늘의 수익 : {income}")
+            print("+ + + + 음료별 판매 현황 + + + +")
+            for x in range(len(name)):
+                print(f"{name[x]} = {10 - stock[x]}개 판매")
             input("부좌 돼롸~ 수ㅖ꺄")
         else:
             print("잘못 눌렀다 새꺄")
